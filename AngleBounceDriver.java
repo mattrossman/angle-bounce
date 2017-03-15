18001c719f560c92d80fe43b570c6c95d8b349a7
Matt Rossman <rossman.m@husky.neu.edu>
2017-03-14 23:51:19 -0400

    Began terrain class

--- AngleBounceDriver.java
+++ AngleBounceDriver.java
@@ -1,11 +1,14 @@
 import java.awt.BorderLayout;
+import java.awt.Point;
+import java.util.Arrays;
+
 import javax.swing.JFrame;
 import javax.swing.JButton;
 
 public class AngleBounceDriver {
 
    public static void main(String[] args){
-   
+      /*
       JFrame window = new JFrame("Physics test");
       window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       window.setLayout(new BorderLayout());
@@ -14,12 +17,8 @@ public class AngleBounceDriver {
       window.add(bouncePanel, BorderLayout.CENTER);
       
       window.setSize(1000,1000);
-      window.setVisible(true);
-      
-      XPoint p1 = new XPoint(20,30);
-      XPoint p2 = new XPoint(30,30);
-      XPoint p3 = new XPoint(20,20);
-      System.out.println(p1.distance(p2,p3));
+      window.setVisible(true);;
+      */
    }
 
 }
--- Terrain.java
+++ Terrain.java
@@ -6,5 +6,29 @@ public class Terrain {
    private ArrayList<Point> mesh = new ArrayList<Point>();
    
    public Terrain(){};
+   
+   public void addPoint(Point p){
+      mesh.add(p);
+   }
+   
+   public int[] getXPoints(){
+   
+      int[] listOfX = new int[mesh.size()];
+      
+      for (int i=0; i<listOfX.length; ++i)
+         listOfX[i] = (int) mesh.get(i).getX();
+      
+      return listOfX;
+   }
+   
+   public int[] getYPoints(){
+   
+      int[] listOfY = new int[mesh.size()];
+      
+      for (int i=0; i<listOfY.length; ++i)
+         listOfY[i] = (int) mesh.get(i).getY();
+      
+      return listOfY;
+   }
 
 }
